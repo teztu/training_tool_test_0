e89acbe6d3432e2944e362f077d3a119382a7f5b
teztu <nicowc@gmail.com>
2025-08-25 22:38:58 +0200

    Created endpoints for GET/HEALTHZ to check if your health is good. A POST/Weightz to upload your current weight, and what date it is. And a GET/Weightz to return rows from (list at the moment( of your weight and date record(

---
+++ README.md
@@ -0,0 +1,14 @@
+# A first attempt at a basic training tool
+
+In this project I am using fast-api to log bodyweight.
+The goal for this project is to learn proper use of:
+
+    Python
+    API 
+    Postgres SQL
+    Docker
+    CI/ID
+
+# How to run:
+```bash
+docker compose up --build
---
+++ app/main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from datetime import date
+
+app= FastAPI()
+
+bodyweights=[]
+
+
+
+class Weight_input(BaseModel):
+
+    day: date
+    kg: float = Field(..., gt=0)
+
+
+class Weight_output(BaseModel):
+    id: int
+    day: date
+    kg: float = Field(..., gt=0)
+
+
+
+@app.get("/healthz")
+def message():
+    return {"msg": "ok"}
+
+
+
+@app.post("/weightz", response_model=Weight_output, status_code=201) #responsemodel forteller hva slags respons man forventer
+def my_weight(new_weight: Weight_input):
+    new_id = len(bodyweights) + 1
+    row = Weight_output(id=new_id, day=new_weight.day, kg=new_weight.kg)
+    bodyweights.append(row)
+
+    #return  ("msg": "You weighed " + str(new_weight.kg) + " kg on date " + str(new_weight.day))
+    return row
+
+
+@app.get("/weightz",  response_model=list[Weight_output])
+def get_weight(limit: int=1000):
+    return bodyweights[-limit:][::-1]
+
+
